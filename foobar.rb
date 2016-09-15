641d66ea8a05b0fdd9c4c03a9c4bce27648f3a90
Alex Shan <Alex Shan>
2016-09-14 20:39:32 -0700

    hw1 solved

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,18 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    nums, sum = Array.new, 0
+    a.map do |elem|
+      elem = (elem.to_i) + 2
+      if elem % 2 == 0 and elem < 10 and !(nums.include? elem)
+        nums.push(elem)
+      end
+    end
+    nums.each do |x|
+      sum += x
+    end
+    return sum
+
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+  return (a+b)*(a+b)
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  a = a.sort
+  a.map do |x|
+    x = x + 1
+  end
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+  return first_name + ' ' + last_name
 end
 
 def blockin_time(a)
